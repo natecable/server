c3ef9d2add4701e6f79b03a0f9aad27a6521bace
Nate Cable <nrc324@lehigh.edu>
2025-04-23 12:54:48 -0400

    up

--- login.php
+++ login.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>(Insert Team Name) Room</title>
+    <title>Team 4 Room</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
